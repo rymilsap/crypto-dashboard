3aa017d90e46735f06052f833e4998b409f3f9c6
unknown <rymilsap@gmail.com>
2024-08-07 17:25:00 -0500

    Update crypto table

--- src/components/CryptoTable.jsx
+++ src/components/CryptoTable.jsx
@@ -19,21 +19,19 @@ function CryptoTable() {
           <th>Market Cap</th>
           <th>Volume(24h)</th>
           <th>Circulating Supply</th>
-          <th>Last 7 days</th>
         </tr>
       </thead>
       <tbody>
         {data.map((coin) => (
           <tr key={coin.id}>
-            <td>{coin.rank}</td>
+            <td>{coin.market_cap_rank}</td>
             <td>{coin.name}</td>
-            <td>${coin.price.toFixed(2)}</td>
-            <td>{coin.change24h.toFixed(2)}%</td>
-            <td>{coin.change7d.toFixed(2)}%</td>
-            <td>${coin.marketCap.toLocaleString()}</td>
-            <td>${coin.volume24h.toLocaleString()}</td>
-            <td>{coin.circulatingSupply.toLocaleString()}</td>
-            <td>[Chart placeholder]</td>
+            <td>${coin.current_price.toFixed(2)}</td>
+            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+            <td>{coin.price_change_percentage_7d_in_currency?.toFixed(2)}%</td>
+            <td>${coin.market_cap.toLocaleString()}</td>
+            <td>${coin.total_volume.toLocaleString()}</td>
+            <td>{coin.circulating_supply.toLocaleString()} {coin.symbol.toUpperCase()}</td>
           </tr>
         ))}
       </tbody>
