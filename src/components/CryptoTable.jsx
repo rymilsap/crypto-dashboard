03e105f46ab1e7abe60b794f4a2547cf16b6e89e
unknown <rymilsap@gmail.com>
2024-08-07 18:47:49 -0500

    added sparklines to table

--- src/components/CryptoTable.jsx
+++ src/components/CryptoTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFetchCryptoData } from '../hooks/useFetchCryptoData';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 function CryptoTable() {
   const { data, loading, error } = useFetchCryptoData();
@@ -19,6 +20,7 @@ function CryptoTable() {
           <th>Market Cap</th>
           <th>Volume(24h)</th>
           <th>Circulating Supply</th>
+          <th>Last 7 Days</th>
         </tr>
       </thead>
       <tbody>
@@ -32,6 +34,11 @@ function CryptoTable() {
             <td>${coin.market_cap.toLocaleString()}</td>
             <td>${coin.total_volume.toLocaleString()}</td>
             <td>{coin.circulating_supply.toLocaleString()} {coin.symbol.toUpperCase()}</td>
+            <td>
+              <Sparklines data={coin.sparkline_in_7d.price} width={100} height={30}>
+                <SparklinesLine color={coin.price_change_percentage_7d > 0 ? "green" : "red"} />
+              </Sparklines>
+            </td>
           </tr>
         ))}
       </tbody>
