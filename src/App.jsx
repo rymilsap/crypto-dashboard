f2fe1fab3bc3b7109cba1b2955c6641fad905f47
unknown <rymilsap@gmail.com>
2024-08-09 02:03:30 -0500

    deleted trending section for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import MarketOverview from './components/MarketOverview';
-import TrendingSection from './components/TrendingSection';
 import CryptoTable from './components/CryptoTable';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <MarketOverview />
-      <TrendingSection />
       <CryptoTable />
     </div>
   );
