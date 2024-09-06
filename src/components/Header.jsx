81e97c4ef97630615753425d573b069c61ff5855
rmmilsap <rmmilsap@crimson.ua.edu>
2024-09-06 12:15:01 -0500

    Added github and linkedin links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        <img src="/logo.png" alt="RYCap" className="logo" />
+        <img src="/logo.png" alt="RyCoinCap" className="logo" />
         <nav>
           <ul>
             <li><a href="#">Cryptocurrencies</a></li>
@@ -12,6 +13,14 @@ const Header = () => {
             <li><a href="#">Portfolio</a></li>
           </ul>
         </nav>
+      </div>
+      <div className="header-right">
+        <a href="https://github.com/rymilsap" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="social-icon" />
+        </a>
+        <a href="https://linkedin.com/in/ryanmilsap/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="social-icon" />
+        </a>
       </div>
     </header>
   );
